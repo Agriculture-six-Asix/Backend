ec7a0946334d67aff142612fb82846ff9dce431b
dankski1337 <simonagis23@gmail.com>
2024-12-07 18:17:47 +0700

    feat: add edit profile for user & delete photo for user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,5 @@
 import { userServices } from "../services/userServices.js";
 import responseError from "../errors/responseError.js";
-import bcrypt from "bcrypt";
 
 async function getUserById(req, res, next) {
     try {
@@ -58,7 +57,71 @@ async function changePassword(req, res, next) {
     }
 }
 
+async function editProfiles(req, res, next) {
+    try {
+        const { id } = req.params;
+        const { fname, lname, username } = req.body;
+        const photo = req.file;
+
+        if (!id) {
+            throw new responseError('ID tidak ditemukan', 400, false);
+        }
+
+        if (!fname || !lname || !username) {
+            throw new responseError('Semua data harus diisi', 400, false);
+        }
+
+        const photoName = photo ? req.newPhotoName : null;
+
+        const user = await userServices.editProfiles(id, fname, lname, username, photoName);
+
+        res.status(200).json({
+            message: 'Profile berhasil diubah',
+            success: true,
+            data: {
+                user
+            }
+        });
+    } catch (error) {
+        if (error.message === 'User tidak ditemukan') {
+            next(new responseError('User tidak ditemukan', 404, false));
+        } else if (error.message === 'Semua data harus diisi') {
+            next(new responseError('Semua data harus diisi', 400, false));
+        } else {
+            console.log(error);
+            next(error);
+        }
+    }
+}
+
+async function deletePhoto(req, res, next) {
+    try {
+        const { id } = req.params;
+        if (!id) {
+            throw new responseError('ID tidak ditemukan', 400, false);
+        }
+
+        const user = await userServices.deleteProfilePhoto(id);
+        
+        res.status(200).json({
+            message: 'Photo berhasil dihapus',
+            success: true,
+            data: {
+                user
+            }
+        });
+    } catch (error) {
+        if (error.message === 'User tidak ditemukan') {
+            next(new responseError('User tidak ditemukan', 404, false));
+        } else {
+            next(error);
+        }
+    }
+}
+
 export const userController = {
     getUserById,
-    changePassword
+    changePassword,
+    editProfiles,
+    deletePhoto
 }
